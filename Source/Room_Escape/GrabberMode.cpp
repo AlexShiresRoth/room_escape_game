47e305fc25004f7c54ae3906e399c948f20c6d1e
Alex Rothenberg <alexroth96@gmail.com>
2019-03-31 20:53:28 -0400

    add grabber mode for default pawn

---
+++ Source/Room_Escape/GrabberMode.cpp
@@ -0,0 +1,4 @@
+// Copyright by Alex Rothenberg
+
+#include "GrabberMode.h"
+
---
+++ Source/Room_Escape/GrabberMode.h
@@ -0,0 +1,17 @@
+// Copyright by Alex Rothenberg
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GrabberMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOM_ESCAPE_API AGrabberMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
